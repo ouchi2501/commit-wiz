07a2095bba5cb8bd07d5cb528bc0b3487b7f9f99
uncle <unclesam2501@gmail.com>
2023-09-07 15:20:42 +0900

    Add support for specifying the Git repository path as a command line flag. Allows users to provide a custom path instead of using the current directory

--- main.go
+++ main.go
@@ -11,16 +11,23 @@ import (
 func main() {
 	// command line flags
 	summaryLength := flag.Int("l", 50, "Number of tokens in summary")
+	path := flag.String("p", "", "Git repository path(default: current directory)")
 	flag.Parse()
 
 	// get current directory
-	currentDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
+	var dir string
+	if path != nil && *path != "" {
+		cwd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		dir = cwd
+	} else {
+		dir = *path
 	}
 
 	// get git diff
-	gitDiff, err := retrieveGitDiff(currentDir)
+	gitDiff, err := retrieveGitDiff(dir)
 	if err != nil {
 		panic(err)
 	}
