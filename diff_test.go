1bfdbee26d9df2f1a0283268f0f3fe7f81ff982c
uncle <unclesam2501@gmail.com>
2023-09-11 19:06:04 +0900

    Add and commit test file to Git in TestRetrieveGitDiff

--- diff_test.go
+++ diff_test.go
@@ -39,13 +39,10 @@ func TestRetrieveGitDiff(t *testing.T) {
 	cmd = exec.Command("git", "add", testFile)
 	cmd.Dir = tempDir
 	err = cmd.Run()
+	gitStatus(tempDir)
 	if err != nil {
 		t.Fatalf("Error adding test file to Git: %v", err)
 	}
-
-	cmd = exec.Command("git", "rm", "--cached", "test.txt")
-	cmd.Dir = tempDir
-	err = cmd.Run()
 
 	cmd = exec.Command("git", "commit", "-m", "Initial commit")
 	cmd.Dir = tempDir
